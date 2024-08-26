8b978592ffea09b248cdf3cb66b037a09c83d990
enockuwumukiza <148784055+enockuwumukiza@users.noreply.github.com>
2024-08-26 20:45:45 +0300

    Finalized API implementation and hosting

--- app.js
+++ app.js
@@ -11,29 +11,6 @@ app.use(express.json());
 
 app.use('/api', schoolRoutes);
 
-
-
-const connection = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  port:process.env.DB_PORT,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME
-});
-
-connection.connect((err) => {
-  if (err) {
-    console.error('Error connecting to the database:', err.message);
-    return;
-  }
-  console.log('Connected to the MySQL database.');
-  connection.end();
-});
-
-app.get('/', (req, res) => {
-  res.send('<h1>School Management API</h1>');
-});
-
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
